8ea34c6135c554086da45a04359c2d8cbf01fa21
hhs <576515301@qq.com>
2023-01-03 01:14:48 +0800

    pinia数据持久化

--- src/main.js
+++ src/main.js
@@ -5,17 +5,19 @@ import 'element-plus/dist/index.css'
 import '@/styles/normalize.css'
 import "./assets/style/reset.css";
 import Icon from "./components/icon.vue";
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
 
 import App from './App.vue'
 import router from './router'
 
 
 const app = createApp(App)
-
-
+const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 
 app
-    .use(createPinia())
+    .use(pinia)
     .use(router)
     .use(ElementPlus)
     .component("Icon", Icon)
--- src/store/user.js
+++ src/store/user.js
@@ -1,19 +1,26 @@
 import { defineStore } from 'pinia'
-import { login } from '@api/user/login.js'
+import { login, userInfo } from '@api/user.js'
 import router from '@router/index'
 
 const useUserStore = defineStore('users', {
+    persist: true,
     state: () => {
         return {
             token: "",
+            userInfo: {}
         }
     },
     actions: {
         async login(data) {
             const token = await login(data);
             this.token = token.token;
-            localStorage.setItem('token', this.token);
+            // localStorage.setItem('token', this.token);
             router.push('/home');
+        },
+        async userInfo(data) {
+            const info = await userInfo(data);
+            this.userInfo = info;
+            // localStorage.setItem('userInfo', JSON.stringify(this.userInfo));
         }
     }
 })
